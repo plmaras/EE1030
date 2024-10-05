18f8f967a013c07c903dc97313d846261af7d79e
Shriram <59959502+plmaras@users.noreply.github.com>
2024-10-05 09:27:07 +0530

    Add files via upload

---
+++ questions/que2/code/points.py
@@ -0,0 +1,74 @@
+#Code by GVV Sharma
+#September 12, 2023
+#Revised July 21, 2024
+#released under GNU GPL
+#Point Vectors
+
+
+import sys                                          #for path to external scripts
+sys.path.insert(0, '/Users/ramaraogolla/Desktop/EE1030/matgeo/codes/CoordGeo')        #path to my scripts
+import numpy as np
+import numpy.linalg as LA
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#local imports
+from line.funcs import *
+from triangle.funcs import *
+from conics.funcs import circ_gen
+
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+
+#Given points
+A = np.array(([3, 1])).reshape(-1,1) 
+B = np.array(([12, -2])).reshape(-1,1)  
+C = np.array(([0,2])).reshape(-1,1)  
+
+
+#Generating all lines
+x_AB = line_gen(A,B)
+x_BC = line_gen(B,C)
+x_CA = line_gen(C,A)
+
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
+
+#Labeling the coordinates
+colors = np.arange(1,4)
+tri_coords = np.block([[A,B,C]])
+plt.scatter(tri_coords[0,:], tri_coords[1,:], c=colors)
+vert_labels = ['A','B','C']
+for i, txt in enumerate(vert_labels):
+    #plt.annotate(txt, # this is the text
+    plt.annotate(f'{txt}\n({tri_coords[0,i]:.2f}, {tri_coords[1,i]:.2f})',
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(25,5), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+# use set_position
+ax = plt.gca()
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+'''
+ax.spines['left'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+'''
+plt.grid() # minor
+plt.axis('equal')
+
+plt.show()
